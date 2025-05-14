ec9e18f65ea63e44ce43fb3b816137f240897f42
akvarelka1na1ohote <anninferno05@mail.ru>
2025-05-14 22:19:13 +0300

    меняю

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-    // Функции для работы с модальным окном
-        function openModal(title) {
-            document.getElementById('modal-title').textContent = title;
-            document.getElementById('modal').style.display = 'flex';
-        }
+// Функции для работы с модальным окном
+function openModal(title) {
+    document.getElementById('modal-title').textContent = title;
+    document.getElementById('modal').style.display = 'flex';
+}
 
-        function closeModal() {
-            document.getElementById('modal').style.display = 'none';
-        }
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
 
-        // Закрытие модального окна при клике вне его
-        window.onclick = function(event) {
-            const modal = document.getElementById('modal');
-            if (event.target == modal) {
-                closeModal();
-            }
-        }
+// Закрытие модального окна при клике вне его
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
